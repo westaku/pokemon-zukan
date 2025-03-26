35963fac720ce84d2d30972aa298a34457d0730e
westaku <westaku@icloud.com>
2025-03-26 18:55:42 +0900

    置き換え

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../app/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Welcome to Repokemon</h1>
+    </main>
+  );
+}
---
+++ src/pages/pokemon/[id].tsx
@@ -0,0 +1,34 @@
+import PokemonDetail from "@/components/PokemonDetail";
+import { GetStaticProps, GetStaticPaths } from "next";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const ids = Array.from({ length: 100 }, (_, i) => String(i + 1));
+  const paths = ids.map((id) => ({
+    params: { id },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  return {
+    props: {
+      pokemonIdNumber: parseInt(params?.id as string, 10),
+    },
+  };
+};
+
+export default function PokemonDetailPage({
+  pokemonIdNumber,
+}: {
+  pokemonIdNumber: number;
+}) {
+  return (
+    <div>
+      <PokemonDetail pokemonIdNumber={pokemonIdNumber} />
+    </div>
+  );
+}
